56023649056fbc1975ead92678deceb4dbcdef1c
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-04-05 15:31:03 +0530

    Initial-Commit

---
+++ src/main/java/com/java/bytecupidsbackend/ByteCupidsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.java.bytecupidsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteCupidsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteCupidsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/bytecupidsbackend/ByteCupidsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.bytecupidsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteCupidsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
