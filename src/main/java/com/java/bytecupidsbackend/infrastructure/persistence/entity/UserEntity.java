a53eab2268d9adde139fc2bc7dbb1521262c26f2
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-04-05 19:19:42 +0530

    defined entities, dtos and models

---
+++ src/main/java/com/java/bytecupidsbackend/infrastructure/persistence/entity/UserEntity.java
@@ -0,0 +1,63 @@
+package com.java.bytecupidsbackend.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")  // Generates a UUID
+    private UUID id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    public UserEntity() {}
+
+    public UserEntity(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
